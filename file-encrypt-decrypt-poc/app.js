51c58a960fe37b52a26658c1836ecc26a96f957a
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-10-16 21:32:22 -0300

    :recycle: usando promisse para solucionar async problems

--- file-encrypt-decrypt-poc/app.js
+++ file-encrypt-decrypt-poc/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const q = require('q');
 
 // crypt info
 const encryptAlgorithm = 'aes-256-ctr';
@@ -9,26 +10,44 @@ const password = 'senha123456';
 const cipher = crypto.createCipher(encryptAlgorithm, password);
 const decipher = crypto.createDecipher(encryptAlgorithm, password);
 
-// file reader
-const fileText = fs.readFileSync('test.txt', 'utf8');
-console.log('Inicio:', fileText);
+readFile('test.txt', 'utf8')
+    .then((fileText) => {
+        console.log('STEP 1 - Without encryption:', fileText);
+        return encrypt(fileText, 'utf8', 'hex')
+    }).then((encrypted) => {
+        console.log('STEP 2 - After encryption', encrypted);
+        return writeFile('encrypted.txt', encrypted)
+    }).then(() => {
+        console.log('STEP 3 - Finish process of encryption');
+        return readFile('encrypted.txt', 'utf8')
+    }).then((fileText) => {
+        console.log('STEP 4 - With encryption:', fileText);
+        return decrypt(fileText, 'hex', 'utf8')
+    }).then((decrypted) => {
+        console.log('STEP 5 - After decryption', decrypted);
+        return writeFile('decrypted.txt', decrypted)
+    }).then(() => console.log('STEP 6 - Finish process of decryption'));
 
-// encrypt
-const crypted = cipher.update(fileText, 'utf8', 'hex');
+function readFile(fileName, encoding){
+    const deferred = q.defer();
+    fs.readFile(fileName, encoding, (err, fileName) => {
+       deferred.resolve(fileName)
+    });
+    return deferred.promise;
+}
 
-writeFile('encrypted.txt',crypted);
+function writeFile(fileName, encryptedText){
+    const deferred = q.defer();
+    fs.writeFile(fileName, encryptedText, () => {
+        deferred.resolve(true)
+    });
+    return deferred.promise;
+}
 
-const decryptedFileText = fs.readFileSync('encrypted.txt', 'utf8');
+function encrypt(text, inputEncoding, outputEncoding) {
+    return cipher.update(text, inputEncoding, outputEncoding);
+}
 
-// decrypt
-const decrypted = decipher.update(decryptedFileText, 'hex', 'utf8');
-
-writeFile('result.txt', decrypted);
-
-function writeFile(fileName, encryptedText) {
-    fs.writeFile(`${fileName}`, encryptedText, (err) => {
-        if(err) {
-            console.log('Error on create file', err);
-        }
-    })
+function decrypt(text, inputEncoding, outputEncoding) {
+    return decipher.update(text, inputEncoding, outputEncoding);
 }
