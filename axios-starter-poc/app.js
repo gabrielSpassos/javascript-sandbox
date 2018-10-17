d6871e41dbc5531de012c859a8ef6e76c991e033
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-10-16 22:42:55 -0300

    :recycle: criando outra chamada via axios

--- axios-starter-poc/app.js
+++ axios-starter-poc/app.js
@@ -5,13 +5,24 @@ const baseUrl = 'https://swapi.co/api/';
 axios.get(baseUrl + 'people/1')
     .then((response) => {
         // handle success
+        console.log('People');
         console.log(response.data);
     })
     .catch((error) => {
         // handle error
-        console.log(error);
+        console.log('Error', error);
     })
     .then(function () {
         // always executed
         console.log('Done!')
+    });
+
+axios({
+    method:'get',
+    url: baseUrl + 'planets/1',
+}).then((response) => {
+        console.log('Planet');
+        console.log(response.data);
+    }).catch((error) => {
+        console.log('Error: ', error)
     });
