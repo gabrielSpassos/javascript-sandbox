599efbae3c0d9d0137b23b0b8db9429c4336fdf5
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-11-11 21:02:27 -0200

    :recycle: removendo route

--- jwt-poc/routes/loginRoute.js
+++ jwt-poc/routes/loginRoute.js
@@ -3,21 +3,6 @@ const Datasource = require('../dao/datasource.js');
 
 module.exports = function (app) {
 
-    app.get('/login', (req, res) => {
-       const datasource = new Datasource();
-
-        const login = req.query.login;
-        const pass = req.query.pass;
-
-        datasource.getUserIdByLoginAndPassword(login, pass)
-            .then((userId) => {
-                console.log('ID', userId);
-                res.render('login', {
-                    userId: userId
-                });
-            });
-    });
-
     app.post('/login', (req, res) => {
         const datasource = new Datasource();
 
