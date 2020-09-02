214b06cb90aafa080db911c303aa633f656106b3
gabriel passos <gabriel.passos@agibank.com.br>
2020-09-02 14:12:22 -0300

    feat: add new date function

--- basics/date.js
+++ basics/date.js
@@ -1,8 +1,18 @@
 const brazilTimezone = 'America/Sao_Paulo';
 
-const getCurrentDate = (timezone) => {
+const getCurrentDateTime = (timezone) => {
     return new Date().toLocaleString('pt-BR', {timeZone: timezone});
 };
 
+const getCurrentDate = () => {
+    const dateTimeFormatter = new Intl.DateTimeFormat('en', {year: 'numeric', month: '2-digit', day: '2-digit'});
+    let currentDate = new Date();
+    let [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormatter.formatToParts(currentDate);
+    return year + '-' + month + '-' + day;
+} 
+
+currentDateTime = getCurrentDateTime(brazilTimezone);
 currentDate = getCurrentDate();
-console.log('Data atual: ', currentDate);
+
+console.log('Data atual: ', currentDateTime);
+console.log('Data atual com formatador: ', currentDate);
