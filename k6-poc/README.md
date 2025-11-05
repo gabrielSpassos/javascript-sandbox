9cfbd10f157a068bc6bdaec1f82e0105b029de88
gabrielSpassos <gabrielsantos45725@gmail.com>
2025-11-05 09:52:04 -0300

    feat: update k6 poc

--- k6-poc/README.md
+++ k6-poc/README.md
@@ -5,6 +5,45 @@
 docker compose up
 ```
 
+### Output
+```
+█ THRESHOLDS 
+ 
+    http_req_duration
+    ✓ 'p(99.9)<800' p(99.9)=258.01ms
+ 
+ 
+█ TOTAL RESULTS 
+ 
+    checks_total.......: 3018    74.890291/s
+    checks_succeeded...: 100.00% 3018 out of 3018
+    checks_failed......: 0.00%   0 out of 3018
+
+    ✓ Post done
+    ✓ response time is acceptable
+    ✓ Crocodiles loaded
+    ✓ status is 403
+
+    CUSTOM
+    login_failures.................: 503    12.481715/s
+
+    HTTP
+    http_req_duration..............: avg=93.48ms  min=19.67ms med=103.82ms max=280.62ms p(90)=165.65ms p(95)=166.2ms
+    { expected_response:true }...: avg=117.08ms min=20.65ms med=161.53ms max=280.62ms p(90)=165.9ms  p(95)=166.4ms
+    http_req_failed................: 25.00% 503 out of 2012
+    http_reqs......................: 2012   49.926861/s
+
+    EXECUTION
+    iteration_duration.............: avg=1.22s    min=1.2s    med=1.21s    max=2.75s    p(90)=1.21s    p(95)=1.21s  
+    iterations.....................: 503    12.481715/s
+    vus............................: 3      min=2           max=20
+    vus_max........................: 20     min=20          max=20
+
+    NETWORK
+    data_received..................: 2.9 MB 73 kB/s
+    data_sent......................: 185 kB 4.6 kB/s
+```
+
 ### References
 
 - https://www.baeldung.com/k6-framework-load-testing
--- k6-poc/stress-test.js
+++ k6-poc/stress-test.js
@@ -7,13 +7,12 @@ let loginFailures = new Counter('login_failures');
 // test configuration
 export const options = {
   stages: [
-    { duration: '10s', target: 20 }, // ramp up to 20 users in 1 minute
-    { duration: '30s', target: 20 }, // hold steady for 3 minutes
-    { duration: '10s', target: 0 },  // ramp down to 0 users in 1 minute
+    { duration: '10s', target: 20 }, // ramp up to 20 users in 10 seconds
+    { duration: '20s', target: 20 }, // hold steady for 20 seconds
+    { duration: '10s', target: 0 },  // ramp down to 0 users in 10 seconds
   ],
   thresholds: {
     http_req_duration: ['p(99.9)<800'], // 99.9% of requests must complete under 800ms
-    http_req_failed: ['rate<0.01']    // less than 1% request failure rate
   }
 };
 
@@ -21,13 +20,13 @@ export default function() {
   
   // simulate diverse user interactions
   const responses = http.batch([
-    ['POST', 'https://test-api.k6.io/auth/basic/login/', JSON.stringify({username:'test',password:'1234'})],
+    ['POST', 'https://quickpizza.grafana.com/api/post', JSON.stringify({name:'lorem',surname:'ipsum'})],
     ['GET', 'https://test-api.k6.io/public/crocodiles/'],
   ]);
 
   // validate multiple endpoint responses
   check(responses[0], { 
-    'Login done': (r) => r.status === 200,
+    'Post done': (r) => r.status === 200,
     'response time is acceptable': (r) => r.timings.duration < 500 
   });
 
@@ -36,14 +35,18 @@ export default function() {
     'response time is acceptable': (r) => r.timings.duration < 500 
   });
 
-  // simulate user activity
-  sleep(1);
-
-  let res = http.post('https://test-api.k6.io/auth/basic/login/', { 
+  let failure_respose = http.post('https://test-api.k6.io/auth/basic/login/', { 
     username: 'test',
     password: 'wrong_password'
   });
-  if (res.status !== 200) {
+  if (failure_respose.status !== 200) {
     loginFailures.add(1); // Increment the counter for failed logins
   }
+  check(failure_respose, {
+    'status is 403': (r) => r.status === 403,
+    'response time is acceptable': (r) => r.timings.duration < 500
+  });
+
+  // simulate user activity
+  sleep(1);
 }
